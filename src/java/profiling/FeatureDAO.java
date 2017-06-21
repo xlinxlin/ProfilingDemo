7bd2b759fec3f846c944d8a5eabdd667a28d3474
xlinxlin <yan.zhou@xlin.me>
2017-06-21 12:12:47 +0200

    Update FeatureDAO.java

--- src/java/profiling/FeatureDAO.java
+++ src/java/profiling/FeatureDAO.java
@@ -14,41 +14,21 @@ import javax.sql.DataSource;
  */
 
 public interface FeatureDAO {
-   /** 
-      * This is the method to be used to initialize
-      * database resources ie. connection.
-   */
+   
    public void setDataSource(DataSource ds);
    
-   /** 
-      * This is the method to be used to create
-      * a record in the Student table.
-   */
+   
    public void create(String name, double value1,double value2,double value3,double value4,double value5);
    
-   /** 
-      * This is the method to be used to list down
-      * a record from the Student table corresponding
-      * to a passed student id.
-   */
+   
    public Feature getFeature(Integer id);
    
-   /** 
-      * This is the method to be used to list down
-      * all the records from the Student table.
-   */
+  
    public List<Feature> listFeatures();
    
-   /** 
-      * This is the method to be used to delete
-      * a record from the Student table corresponding
-      * to a passed student id.
-   */
+   
    public void delete(Integer id);
    
-   /** 
-      * This is the method to be used to update
-      * a record into the Student table.
-   */
+   
    public void update(Integer id, double value1,double value2,double value3,double value4,double value5);
 }
